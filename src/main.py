7d3bb1a92c141a0878789eb5feb585b376f496e2
yuvaraj-i <yuvarajyosef@gmail.com>
2022-07-09 12:45:29 +0530

    [Add] nlp tokens & segementation & entity

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import spacy
+
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
+
+text = ""
+
+# Process whole documents
+with open("testfile.txt", "r") as f:
+        text = f.read()
+
+doc = nlp(text)
+
+# Find phrases and concepts
+for word in doc:
+        if(word.pos_ == "VERB" or word.pos_ == "NOUN"):
+                print(word, spacy.explain(word.pos_),)
+
+# Find named entities
+for word in doc.ents:
+        print(word.text, word.label_, sep="-")
+
+
